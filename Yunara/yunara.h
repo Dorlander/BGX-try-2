6a5591d6bb2a455d9d698e157b9a8c984791a56f
Dorlander <61416392+Dorlander@users.noreply.github.com>
2025-08-09 02:35:10 +0300

    Update yunara.h

--- Yunara/yunara.h
+++ Yunara/yunara.h
@@ -1,10 +1,10 @@
 ﻿#pragma once
 #include "../plugin_sdk/plugin_sdk.hpp"
 
-// Yunara namespace for all code
+
 namespace yunara
 {
-    // Запуск и выгрузка
+    
     void load();
     void unload();
 }
