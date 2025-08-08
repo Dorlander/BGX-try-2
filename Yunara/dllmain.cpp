d9b02e9ea85779115ca41537360ce19b9a93e495
Dorlander <61416392+Dorlander@users.noreply.github.com>
2025-08-09 02:34:49 +0300

    Update dllmain.cpp

--- Yunara/dllmain.cpp
+++ Yunara/dllmain.cpp
@@ -1,14 +1,14 @@
 ﻿#include "../plugin_sdk/plugin_sdk.hpp"
 #include "yunara.h"
 
-// Отображаемся в списке плагинов
+
 PLUGIN_NAME("Yunara");
 
-// Говорим загрузчику, что это именно misc-плагин
+
 PLUGIN_TYPE(plugin_type::misc);
 
 extern "C" PLUGIN_API bool on_sdk_load(plugin_sdk_core* plugin_sdk_good) {
-    // без этого все глобальные указатели (console, state, renderer…) останутся nullptr
+   
     DECLARE_GLOBALS(plugin_sdk_good);
 
   
